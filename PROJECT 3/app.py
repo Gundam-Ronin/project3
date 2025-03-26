3825410fd32e7575d69d49ac8b310ad920d8f77a
Anthony Tavarez <tavarezanthony187@gmail.com>
2025-03-25 22:48:28 -0500

    Update app.py

--- PROJECT 3/app.py
+++ PROJECT 3/app.py
@@ -9,7 +9,7 @@ from contextlib import contextmanager
 
 # Configuration
 CSV_FILE_PATH = "space_missions.csv"  # use relative path
-DATABASE_URL = os.getenv("postgresql://launches_db_user:GZpMv0pEPb5HUMWZEZyETL96vKacbkkS@dpg-cvhmk4btq21c73flhg1g-a:5432/launches_db")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://launches_db_user:GZpMv0pEPb5HUMWZEZyETL96vKacbkkS@dpg-cvhmk4btq21c73flhg1g-a.oregon-postgres.render.com:5432/launches_db")
 
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
